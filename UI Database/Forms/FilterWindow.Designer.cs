b5c880ab28134d25176c7d4b1dd2567fab78c451
MeretinVV <46193772+MeretinVV@users.noreply.github.com>
2020-05-28 23:26:44 +0300

    Add files via upload

---
+++ UI Database/Forms/FilterWindow.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace UI_Database.Forms
+{
+    partial class FilterWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sexFilterField = new System.Windows.Forms.ComboBox();
+            this.hireDateFilter = new System.Windows.Forms.CheckBox();
+            this.sexFilter = new System.Windows.Forms.CheckBox();
+            this.lastNameFilter = new System.Windows.Forms.CheckBox();
+            this.departmentIDFilter = new System.Windows.Forms.CheckBox();
+            this.cancelFilterChangesBtn = new System.Windows.Forms.Button();
+            this.applyFilterBtn = new System.Windows.Forms.Button();
+            this.departmentIDFilterField = new System.Windows.Forms.ComboBox();
+            this.hireDateFilterField = new System.Windows.Forms.DateTimePicker();
+            this.lastNameFilterField = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sexFilterField
+            // 
+            this.sexFilterField.Enabled = false;
+            this.sexFilterField.FormattingEnabled = true;
+            this.sexFilterField.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.sexFilterField.Location = new System.Drawing.Point(136, 86);
+            this.sexFilterField.Name = "sexFilterField";
+            this.sexFilterField.Size = new System.Drawing.Size(150, 21);
+            this.sexFilterField.TabIndex = 25;
+            // 
+            // hireDateFilter
+            // 
+            this.hireDateFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.hireDateFilter.Location = new System.Drawing.Point(12, 122);
+            this.hireDateFilter.Name = "hireDateFilter";
+            this.hireDateFilter.Size = new System.Drawing.Size(95, 21);
+            this.hireDateFilter.TabIndex = 24;
+            this.hireDateFilter.Text = "Hire Date";
+            this.hireDateFilter.UseVisualStyleBackColor = true;
+            this.hireDateFilter.CheckedChanged += new System.EventHandler(this.FilterOption_CheckedChanged);
+            // 
+            // sexFilter
+            // 
+            this.sexFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sexFilter.Location = new System.Drawing.Point(12, 86);
+            this.sexFilter.Name = "sexFilter";
+            this.sexFilter.Size = new System.Drawing.Size(95, 21);
+            this.sexFilter.TabIndex = 23;
+            this.sexFilter.Text = "Sex";
+            this.sexFilter.UseVisualStyleBackColor = true;
+            this.sexFilter.CheckedChanged += new System.EventHandler(this.FilterOption_CheckedChanged);
+            // 
+            // lastNameFilter
+            // 
+            this.lastNameFilter.AutoSize = true;
+            this.lastNameFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lastNameFilter.Location = new System.Drawing.Point(12, 47);
+            this.lastNameFilter.Name = "lastNameFilter";
+            this.lastNameFilter.Size = new System.Drawing.Size(95, 21);
+            this.lastNameFilter.TabIndex = 22;
+            this.lastNameFilter.Text = "Last Name";
+            this.lastNameFilter.UseVisualStyleBackColor = true;
+            this.lastNameFilter.CheckedChanged += new System.EventHandler(this.FilterOption_CheckedChanged);
+            // 
+            // departmentIDFilter
+            // 
+            this.departmentIDFilter.AutoSize = true;
+            this.departmentIDFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.departmentIDFilter.Location = new System.Drawing.Point(12, 12);
+            this.departmentIDFilter.Name = "departmentIDFilter";
+            this.departmentIDFilter.Size = new System.Drawing.Size(118, 21);
+            this.departmentIDFilter.TabIndex = 21;
+            this.departmentIDFilter.Text = "Department ID";
+            this.departmentIDFilter.UseVisualStyleBackColor = true;
+            this.departmentIDFilter.CheckedChanged += new System.EventHandler(this.FilterOption_CheckedChanged);
+            // 
+            // cancelFilterChangesBtn
+            // 
+            this.cancelFilterChangesBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.cancelFilterChangesBtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.cancelFilterChangesBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cancelFilterChangesBtn.Location = new System.Drawing.Point(163, 160);
+            this.cancelFilterChangesBtn.Name = "cancelFilterChangesBtn";
+            this.cancelFilterChangesBtn.Size = new System.Drawing.Size(123, 30);
+            this.cancelFilterChangesBtn.TabIndex = 20;
+            this.cancelFilterChangesBtn.Text = "Cancel";
+            this.cancelFilterChangesBtn.UseVisualStyleBackColor = false;
+            this.cancelFilterChangesBtn.Click += new System.EventHandler(this.CancelFilterChangesBtn_Click);
+            // 
+            // applyFilterBtn
+            // 
+            this.applyFilterBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.applyFilterBtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.applyFilterBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.applyFilterBtn.Location = new System.Drawing.Point(7, 160);
+            this.applyFilterBtn.Name = "applyFilterBtn";
+            this.applyFilterBtn.Size = new System.Drawing.Size(123, 30);
+            this.applyFilterBtn.TabIndex = 19;
+            this.applyFilterBtn.Text = "Apply";
+            this.applyFilterBtn.UseVisualStyleBackColor = false;
+            this.applyFilterBtn.Click += new System.EventHandler(this.ApplyFilterBtn_Click);
+            // 
+            // departmentIDFilterField
+            // 
+            this.departmentIDFilterField.Enabled = false;
+            this.departmentIDFilterField.FormattingEnabled = true;
+            this.departmentIDFilterField.Location = new System.Drawing.Point(136, 12);
+            this.departmentIDFilterField.Name = "departmentIDFilterField";
+            this.departmentIDFilterField.Size = new System.Drawing.Size(150, 21);
+            this.departmentIDFilterField.TabIndex = 18;
+            // 
+            // hireDateFilterField
+            // 
+            this.hireDateFilterField.Enabled = false;
+            this.hireDateFilterField.Location = new System.Drawing.Point(136, 123);
+            this.hireDateFilterField.Name = "hireDateFilterField";
+            this.hireDateFilterField.Size = new System.Drawing.Size(150, 20);
+            this.hireDateFilterField.TabIndex = 17;
+            // 
+            // lastNameFilterField
+            // 
+            this.lastNameFilterField.Enabled = false;
+            this.lastNameFilterField.Location = new System.Drawing.Point(136, 47);
+            this.lastNameFilterField.Name = "lastNameFilterField";
+            this.lastNameFilterField.Size = new System.Drawing.Size(150, 20);
+            this.lastNameFilterField.TabIndex = 16;
+            // 
+            // FilterWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(299, 202);
+            this.Controls.Add(this.sexFilterField);
+            this.Controls.Add(this.hireDateFilter);
+            this.Controls.Add(this.sexFilter);
+            this.Controls.Add(this.lastNameFilter);
+            this.Controls.Add(this.departmentIDFilter);
+            this.Controls.Add(this.cancelFilterChangesBtn);
+            this.Controls.Add(this.applyFilterBtn);
+            this.Controls.Add(this.departmentIDFilterField);
+            this.Controls.Add(this.hireDateFilterField);
+            this.Controls.Add(this.lastNameFilterField);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FilterWindow";
+            this.Text = "FilterWindow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox sexFilterField;
+        private System.Windows.Forms.CheckBox hireDateFilter;
+        private System.Windows.Forms.CheckBox sexFilter;
+        private System.Windows.Forms.CheckBox lastNameFilter;
+        private System.Windows.Forms.CheckBox departmentIDFilter;
+        private System.Windows.Forms.Button cancelFilterChangesBtn;
+        private System.Windows.Forms.Button applyFilterBtn;
+        private System.Windows.Forms.ComboBox departmentIDFilterField;
+        private System.Windows.Forms.DateTimePicker hireDateFilterField;
+        private System.Windows.Forms.TextBox lastNameFilterField;
+    }
+}
---
+++ UI Database/Forms/MainForm.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+using UI_Database.Events;
+using UI_Database.Forms;
+using System.Data.Linq;
+
+namespace UI_Database 
+{
+    public partial class MainForm : Form
+    {
+        private SqlConnectionStringBuilder connectionStrBuilder;
+        private FilterChangeEventArgs _currentFilterArgs = new FilterChangeEventArgs();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            ConnectToDatabase();
+        }
+
+        private void ConnectToDatabase()
+        {
+            connectionStrBuilder = new SqlConnectionStringBuilder();
+            connectionStrBuilder.IntegratedSecurity = true;
+            connectionStrBuilder.DataSource = @"(LocalDB)\MSSQLLocalDB";
+            connectionStrBuilder.AttachDBFilename =
+                Path.GetFullPath(@"C:\Users\Jack\Desktop\Технология разработки\12. UI Database\UI Database\Database.mdf");
+        }
+
+        private void peopleBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.peopleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.databaseDataSet);
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "databaseDataSet.People". При необходимости она может быть перемещена или удалена.
+            this.peopleTableAdapter.Fill(this.databaseDataSet.People);
+
+        }
+
+
+        private void LoadXmlBtn_Click(object sender, EventArgs e)
+        {
+            DataSet data = new DataSet();
+            data.ReadXml("Data.xml");
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionStrBuilder.ToString();
+                try
+                {
+                    connection.Open();
+
+                    // Удаляем что было до этого в базе данных
+                    string strSQL = "DELETE FROM People";
+                    SqlCommand command = new SqlCommand(strSQL, connection);
+                    command.ExecuteNonQuery();
+
+                    // Добавляем новые элементы из xml файла
+                    foreach (DataRow row in data.Tables[0].Rows)
+                    {
+                        List<string> vals = new List<string>();
+                        foreach (string v in row.ItemArray) vals.Add($"'{v}'");
+
+                        string values = string.Join(", ", vals);
+                        strSQL =
+                            $@"INSERT INTO People
+                            VALUES ({values})";
+                        command = new SqlCommand(strSQL, connection);
+                        command.ExecuteNonQuery();
+                    }
+
+                    UpdateGrid();
+                }
+
+                catch (SqlException ex)
+                {
+                    MessageBox.Show($"Something went wrong while reading XML file: {ex.Message}");
+                }
+            }
+        }
+
+        private void ClearDatabaseBtn_Click(object sender, EventArgs e)
+        {
+            DataSet data = new DataSet();
+            data.ReadXml("Data.xml");
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionStrBuilder.ToString();
+                try
+                {
+                    connection.Open();
+
+                    string strSQL = "DELETE FROM People";
+                    SqlCommand command = new SqlCommand(strSQL, connection);
+                    command.ExecuteNonQuery();
+
+                    UpdateGrid();
+                }
+                
+                catch (SqlException ex)
+                {
+                    MessageBox.Show($"Something went wrong while clearing the database: {ex.Message}");
+                }
+            }
+        }
+
+
+        // Вот в этом методе должно происходить обновление таблицы, чтобы она
+        // выглядела как текущее состояние базы данных
+        private void UpdateGrid()
+        {
+            this.peopleTableAdapter.Fill(this.databaseDataSet.People);
+            peopleBindingSource.ResetBindings(false);
+
+            //this.peopleBindingSource.EndEdit();
+            //this.tableAdapterManager.UpdateAll(this.databaseDataSet);
+            //peopleBindingSource.DataSource = this.databaseDataSet.People;
+            //peopleDataGridView.DataSource = null;
+            peopleDataGridView.DataSource = peopleBindingSource.DataSource;
+            peopleDataGridView.Refresh();
+
+
+        }
+
+        private void setFilterBtn_Click(object sender, EventArgs e)
+        {
+            using (DataClassesDataContext dc = new DataClassesDataContext(connectionStrBuilder.ToString()))
+            {
+                var departments = dc.People.GroupBy(x => x.Department_ID)
+                                  .Select(y => y.First().Department_ID.ToString()).ToList();
+
+                
+                var filterWindow = new FilterWindow(departments, _currentFilterArgs);
+                filterWindow.FilterChangeEvent += new EventHandler<FilterChangeEventArgs>(this.OnFilterChange);
+                filterWindow.ShowDialog();
+                
+            }
+        }
+
+        private void OnFilterChange(object sender, FilterChangeEventArgs e)
+        {
+            _currentFilterArgs = e;
+            ApplyFilter();
+            UpdateGrid();
+        }
+
+        // В этом методе проблема с преобразованием
+        private void ApplyFilter()
+        {
+            using (DataClassesDataContext dc = new DataClassesDataContext(connectionStrBuilder.ToString()))
+            {
+                try
+                {
+                    // Это отфильтрованные данные, но я не очень понимаю как их отобразить
+                    // явные преобразования в DataSet или DataTable не работают.
+                    var filteredData = 
+                        from person in dc.People
+                        where person.Department_ID.ToString().Contains(_currentFilterArgs.DepartmentID)
+                        && person.Last_Name.ToLower().Contains(_currentFilterArgs.LastName.ToLower())
+                        && person.Hire_Date.Contains(_currentFilterArgs.HireDate)
+                        && person.Sex.Contains(_currentFilterArgs.Sex)
+                        select person;
+                    
+
+                    //this.peopleTableAdapter.Fill();
+                    //peopleDataGridView.DataSource = filteredData;
+                }
+                catch (NullReferenceException)
+                {
+                    throw new FormatException("The data have incorrect format. Some entries might be missing " +
+                        "at least one of the following key elements: Employee_ID, Department_ID, Last_Name, Hire_Date, Sex.");
+                }
+                
+            }
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            UpdateGrid();
+        }
+    }
+}
