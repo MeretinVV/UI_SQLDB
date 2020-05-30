da727a921ddf9bbdfd27a18773f19e024cccef41
MeretinVV <46193772+MeretinVV@users.noreply.github.com>
2020-05-30 17:47:12 +0300

    Add files via upload

---
+++ UI Database/Forms/MainWindow.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace UI_Database
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.filterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setFilterBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.LoadXmlBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.ClearDatabaseBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.peopleDataGridView = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.peopleDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filterToolStripMenuItem,
+            this.LoadXmlBtn,
+            this.ClearDatabaseBtn});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(484, 27);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // filterToolStripMenuItem
+            // 
+            this.filterToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setFilterBtn,
+            this.clearBtn});
+            this.filterToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.filterToolStripMenuItem.Name = "filterToolStripMenuItem";
+            this.filterToolStripMenuItem.Size = new System.Drawing.Size(51, 23);
+            this.filterToolStripMenuItem.Text = "Filter";
+            // 
+            // setFilterBtn
+            // 
+            this.setFilterBtn.Name = "setFilterBtn";
+            this.setFilterBtn.Size = new System.Drawing.Size(128, 24);
+            this.setFilterBtn.Text = "Set filter";
+            this.setFilterBtn.Click += new System.EventHandler(this.setFilterBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(128, 24);
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // LoadXmlBtn
+            // 
+            this.LoadXmlBtn.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.LoadXmlBtn.Name = "LoadXmlBtn";
+            this.LoadXmlBtn.Size = new System.Drawing.Size(83, 23);
+            this.LoadXmlBtn.Text = "Load XML";
+            this.LoadXmlBtn.Click += new System.EventHandler(this.LoadXmlBtn_Click);
+            // 
+            // ClearDatabaseBtn
+            // 
+            this.ClearDatabaseBtn.Name = "ClearDatabaseBtn";
+            this.ClearDatabaseBtn.Size = new System.Drawing.Size(97, 23);
+            this.ClearDatabaseBtn.Text = "Clear Database";
+            this.ClearDatabaseBtn.Click += new System.EventHandler(this.ClearDatabaseBtn_Click);
+            // 
+            // peopleDataGridView
+            // 
+            this.peopleDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.peopleDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.peopleDataGridView.Location = new System.Drawing.Point(12, 30);
+            this.peopleDataGridView.Name = "peopleDataGridView";
+            this.peopleDataGridView.Size = new System.Drawing.Size(460, 319);
+            this.peopleDataGridView.TabIndex = 7;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.peopleDataGridView);
+            this.Controls.Add(this.menuStrip1);
+            this.MinimumSize = new System.Drawing.Size(500, 400);
+            this.Name = "MainForm";
+            this.Text = "MainWindow";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.peopleDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem filterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setFilterBtn;
+        private System.Windows.Forms.ToolStripMenuItem clearBtn;
+        private System.Windows.Forms.ToolStripMenuItem LoadXmlBtn;
+        private System.Windows.Forms.ToolStripMenuItem ClearDatabaseBtn;
+        private System.Windows.Forms.DataGridView peopleDataGridView;
+    }
+}
+
---
+++ UI Database/Forms/MainWindow.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+using UI_Database.Events;
+using UI_Database.Forms;
+using System.Data.Linq;
+
+namespace UI_Database
+{
+    public partial class MainForm : Form
+    {
+        protected SqlConnection _connection;
+        protected FilterChangeEventArgs _currentFilterArgs = new FilterChangeEventArgs();
+        protected DataClassesDataContext _dc;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            ConnectToDatabase();
+        }
+        /// <summary>
+        /// Establishes connection to the database
+        /// </summary>
+        private void ConnectToDatabase()
+        {
+            string dbPath = @"C:\Users\Jack\Desktop\Технология разработки\12. UI Database\UI Database\Database.mdf";
+            _connection = new SqlConnection($@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={dbPath};Integrated Security=True");
+            _dc = new DataClassesDataContext(_connection);
+        }
+
+        /// <summary>
+        /// Load data from .xml file to the database
+        /// </summary>
+        private void LoadXmlBtn_Click(object sender, EventArgs e)
+        {
+            DataSet data = new DataSet();
+            data.ReadXml("Data.xml");
+
+            try
+            {
+               _connection.Open();
+
+                // Удаляем что было до этого в базе данных
+                string strSQL = "DELETE FROM People";
+                SqlCommand command = new SqlCommand(strSQL, _connection);
+                command.ExecuteNonQuery();
+
+                // Добавляем новые элементы из xml файла
+                foreach (DataRow row in data.Tables[0].Rows)
+                {
+                    List<string> vals = new List<string>();
+                    foreach (string v in row.ItemArray) vals.Add($"'{v}'");
+
+                    string values = string.Join(", ", vals);
+                    strSQL =
+                        $@"INSERT INTO People
+                        VALUES ({values})";
+                    command = new SqlCommand(strSQL, _connection);
+                    command.ExecuteNonQuery();
+                }
+
+                _connection.Close();
+                UpdateGrid();
+            }
+
+            catch (SqlException ex)
+            {
+                MessageBox.Show($"Something went wrong while reading XML file: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Clears database
+        /// </summary>
+        private void ClearDatabaseBtn_Click(object sender, EventArgs e)
+        {
+            DataSet data = new DataSet();
+            data.ReadXml("Data.xml");
+
+            try
+            {
+                _connection.Open();
+
+                string strSQL = "DELETE FROM People";
+                SqlCommand command = new SqlCommand(strSQL, _connection);
+                command.ExecuteNonQuery();
+                
+                _connection.Close();
+                UpdateGrid();
+            }
+
+            catch (SqlException ex)
+            {
+                MessageBox.Show($"Something went wrong while clearing the database: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Refreshes data in DataGridView to represent current condition of the database
+        /// </summary>
+        private void UpdateGrid()
+        {
+            _dc = new DataClassesDataContext(_connection);
+            peopleDataGridView.DataSource = _dc.People;
+        }
+
+        /// <summary>
+        /// Opens filter window
+        /// </summary>
+        private void setFilterBtn_Click(object sender, EventArgs e)
+        {
+            var departments = _dc.People.GroupBy(x => x.Department_ID)
+                                .Select(y => y.First().Department_ID.ToString()).ToList();
+
+            var filterWindow = new FilterWindow(departments, _currentFilterArgs);
+            filterWindow.FilterChangeEvent += new EventHandler<FilterChangeEventArgs>(this.OnFilterChange);
+            filterWindow.ShowDialog();
+        }
+
+        /// <summary>
+        /// React to event from filter menu
+        /// </summary>
+        private void OnFilterChange(object sender, FilterChangeEventArgs e)
+        {
+            _currentFilterArgs = e;
+            ApplyFilter();
+        }
+
+        /// <summary>
+        /// Applies filter and shows result in DataGridView
+        /// </summary>
+        private void ApplyFilter()
+        {
+            try
+            {
+                peopleDataGridView.DataSource = _dc.People.Where(person =>
+                    person.Department_ID.ToString().Contains(_currentFilterArgs.DepartmentID)
+                    && person.Last_Name.ToLower().Contains(_currentFilterArgs.LastName.ToLower())
+                    && person.Hire_Date.Contains(_currentFilterArgs.HireDate)
+                    && person.Sex.Contains(_currentFilterArgs.Sex));
+            }
+            catch (NullReferenceException)
+            {
+                throw new FormatException("The data have incorrect format. Some entries might be missing " +
+                    "at least one of the following key elements: Employee_ID, Department_ID, Last_Name, Hire_Date, Sex.");
+            }
+
+        }
+
+        /// <summary>
+        /// Clears filter
+        /// </summary>
+        private void clearBtn_Click(object sender, EventArgs e) => UpdateGrid();
+
+    }
+}
